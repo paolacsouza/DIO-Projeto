359b9596c79cd566e94c3d9c27618f184fa83914
Paola de Souza Cipriano <154745947+paolacsouza@users.noreply.github.com>
2024-03-29 16:03:58 -0300

    Add files via upload

---
+++ Projeto DIO.py
@@ -0,0 +1,10 @@
+#Python
+def journey_hero(posicao_inicial, total_passos):
+    posicao_final = posicao_inicial + total_passos
+    return posicao_final
+print('Posição final do heroi:')
+print(journey_hero(2,3))
+print('Posição final do heroi:')
+print(journey_hero(15,3))
+print('Posição final do heroi:')
+print(journey_hero(10,6))
